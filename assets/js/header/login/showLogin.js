a08bde6e33d1852b67ff0d244612442948669dfc
NGUYEN CONG MINH <84337695+ECONGMINH@users.noreply.github.com>
2021-08-26 10:23:56 +0700

    Update showLogin.js

--- assets/js/header/login/showLogin.js
+++ assets/js/header/login/showLogin.js
@@ -286,7 +286,7 @@ function showLogin(){
     function start(){
         handleIntoLogin();
         handleIntoCreate();
-        handleBtnTrolley();
+//         handleBtnTrolley();
     }
     start();
 
